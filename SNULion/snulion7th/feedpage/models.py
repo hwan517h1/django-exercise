c592b2278a4c715115cecf5aa8d5b15149a51a94
이경환 <hwan517h0@MacBookPro.local>
2019-04-02 02:22:17 +0900

    수정까지 구현함

---
+++ SNULion/snulion7th/feedpage/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+from faker import Faker
+
+class Feed(models.Model):
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    def seed(count):
+        myfaker = Faker('ko_KR')
+        for i in range(count):
+            Feed.objects.create(
+                title = myfaker.bs(),
+                content = myfaker.text(),
+            )
+
+    def update_date(self):
+        self.updated_at = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
